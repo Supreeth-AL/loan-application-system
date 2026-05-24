a6765af25b67260680426a156c9c1aeddcea6a95
Supreeth A L <supreethal40@gmail.com>
2026-05-24 19:19:27 +0530

    update the homepage ui

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,9 +12,9 @@ function HomePage() {
 
   return (
     <div className="bg-white p-10 rounded-xl shadow-md text-center">
-      <h2 className="text-3xl font-bold text-blue-600 mb-4">
-        Welcome to Loan Portal
-      </h2>
+      <h2 style={{ color: "#2563eb" }} className="text-3xl font-bold mb-4">
+  Welcome to Loan Portal
+</h2>
 
       <p className="text-gray-600 mb-6">
         Apply for your loan using our secure multi-step application form.
@@ -22,14 +22,14 @@ function HomePage() {
 
       <Link
         to="/loan-form"
-        className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700"
+        className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 mt-8 "
       >
         Start Application
       </Link>
 
       <button
         onClick={handleClearData}
-        className="bg-red-500 text-white px-6 py-3 rounded-lg hover:bg-red-600 ml-4"
+        className="bg-red-500 text-white px-6 py-3 rounded-lg hover:bg-red-600 ml-4 mt-8"
       >
         Clear Saved Data
       </button>
